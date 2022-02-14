4578e26e9088b0d6137a8f9de3914041e1fb76d3
The Lord <egortuisov@gmail.com>
2022-02-14 17:06:41 +0500

    part 5 + 6

--- classes.py
+++ classes.py
@@ -2,18 +2,56 @@ import pygame
 
 
 class Button(pygame.sprite.Sprite):
-
-    def __init__(self, pos, text, *groups):
+    def __init__(self, pos, size, text, *groups):
         super().__init__(*groups)
-        font = pygame.font.Font(None, 50)
-        self.image = font.render(text, False, (0, 0, 0), (230, 230, 230))
-        self.rect = self.image.get_rect().move(*pos)
+        self.font = pygame.font.Font(None, 50)
+        self.image = pygame.Surface(size)
+        self.rect = pygame.Rect(*pos, *size)
+        self.change_text(text)
 
     def clicked(self, pos):
         return self.rect.collidepoint(*pos)
 
     def change_text(self, text):
-        font = pygame.font.Font(None, 50)
-        self.image = font.render(text, False, (0, 0, 0), (230, 230, 230))
-        self.rect.width = self.image.get_width()
+        self.image.fill((230, 230, 230))
+        text = self.font.render(text, False, (0, 0, 0))
+        pos = (self.rect.width - text.get_width()) / 2, (self.rect.height - text.get_height()) / 2
+        self.image.blit(text, pos)
 
+
+class ImageButton(pygame.sprite.Sprite):
+    def __init__(self, pos, size, image, *groups):
+        super().__init__(*groups)
+        self.image = pygame.transform.scale(pygame.image.load(image), size)
+        self.rect = pygame.Rect(*pos, *size)
+
+    def clicked(self, pos):
+        return self.rect.collidepoint(*pos)
+
+
+class EditText(pygame.sprite.Sprite):
+    def __init__(self, pos, size, font_size, *groups):
+        super().__init__(*groups)
+        self.font = pygame.font.Font(None, font_size)
+        self.image = pygame.Surface(size)
+        self.image.fill((230, 230, 230))
+        self.rect = pygame.Rect(*pos, *size)
+        self.text = ""
+
+    def clicked(self, pos):
+        return self.rect.collidepoint(*pos)
+
+    def render(self):
+        self.image.fill((230, 230, 230))
+        text = self.font.render(self.text, False, (0, 0, 0))
+        pos = (self.rect.width - text.get_width()) / 2, (self.rect.height - text.get_height()) / 2
+        self.image.blit(text, pos)
+
+    def update(self, event, *args):
+        if event.type == pygame.TEXTINPUT:
+            self.text += event.text
+            self.render()
+        elif event.type == pygame.KEYDOWN and event.key == pygame.K_BACKSPACE:
+            if self.text:
+                self.text = self.text[:-1]
+                self.render()
--- main.py
+++ main.py
@@ -4,23 +4,27 @@ import sys
 import pygame
 import requests
 from io import BytesIO
-from classes import Button
+from classes import Button, EditText, ImageButton
+import pygame.examples
 
 static_api = "http://static-maps.yandex.ru/1.x/"
-geocoder_api = ""
-geokey = ""
+geocoder_api = "http://geocode-maps.yandex.ru/1.x/"
+geokey = "40d1649f-0493-4b70-98ba-98533de7710b"
 SCREEN_SIZE = SCREEN_W, SCREEN_H = 650, 650
 spn = 30
 center = [0, 0]
 map_type = 0
 maps = ["map", "sat", "sat,skl"]
 maps_names = ["Схема", "Спутник", "Гибрид"]
+default_point = "{},pm2ntl"
+points = []
 motion = {pygame.K_UP: (0, 0.5), pygame.K_DOWN: (0, -0.5),
           pygame.K_LEFT: (-1, 0), pygame.K_RIGHT: (1, 0)}
 pygame.init()
 screen = pygame.display.set_mode(SCREEN_SIZE)
 screen.fill((155, 155, 155))
 all_sprites = pygame.sprite.Group()
+print(pygame.examples.__file__)
 
 
 def get_map_png(ll=None, spn=None, pts=[], l="map"):
@@ -34,6 +38,7 @@ def get_map_png(ll=None, spn=None, pts=[], l="map"):
         params["spn"] = ",".join(map(str, spn))
     if pts:
         params["pt"] = "~".join(pts)
+    print(pts)
     response = requests.get(static_api, params=params)
     if not response:
         print("Ошибка выполнения запроса:")
@@ -45,14 +50,41 @@ def get_map_png(ll=None, spn=None, pts=[], l="map"):
 
 def draw_map():
     screen.fill((155, 155, 155))
-    map_file = get_map_png(ll=center, spn=(spn, spn), l=maps[map_type])
+    map_file = get_map_png(ll=center, spn=(spn, spn), l=maps[map_type], pts=points)
     screen.blit(map_file, (0, 0))
     all_sprites.draw(screen)
 
 
+def calc_coords(geoobj):
+    ll = geoobj["Point"]["pos"].split()
+    envelope = geoobj["boundedBy"]["Envelope"]
+    l, b = envelope["lowerCorner"].split(" ")
+    r, t = envelope["upperCorner"].split(" ")
+    dx = abs(float(l) - float(r)) / 2.0
+    return ll, float(dx)
+
+
+def get_coords(name):
+    response = requests.get(geocoder_api, params={
+        "apikey": geokey,
+        "geocode": name,
+        "format": "json"
+    })
+    if not response:
+        print("Ничего не найдено")
+        print(response.url)
+        exit(0)
+    json_response = response.json()
+    if not json_response["response"]["GeoObjectCollection"]["featureMember"]:
+        return center, spn
+    return calc_coords(json_response["response"]["GeoObjectCollection"]["featureMember"][0]["GeoObject"])
+
+
 def main():
-    global spn, center, map_type
-    map_type_btn = Button((0, 452), "Схема", all_sprites)
+    global spn, center, map_type, points
+    map_type_btn = Button((2, 452), (200, 40), "Схема", all_sprites)
+    req_et = EditText((2, 494), (SCREEN_W - 54, 40), 30, all_sprites)
+    search_btn = ImageButton((SCREEN_W - 42, 494), (40, 40), "search.png", all_sprites)
     draw_map()
     running = True
     while running:
@@ -86,6 +118,13 @@ def main():
                     map_type = (map_type + 1) % len(maps)
                     map_type_btn.change_text(maps_names[map_type])
                     draw_map()
+                if search_btn.clicked(event.pos):
+                    points.clear()
+                    center, spn = get_coords(req_et.text)
+                    points.append(default_point.format(",".join(map(str, center))))
+                    draw_map()
+            req_et.update(event)
+        all_sprites.draw(screen)
         pygame.display.flip()
 
     pygame.quit()
